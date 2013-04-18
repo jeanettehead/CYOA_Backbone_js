195862e17a58d2a6ca53978d3318690134955717
= <=>
2013-04-18 13:36:40 -0400

    changes

--- scripts/models/question.js
+++ scripts/models/question.js
@@ -1,3 +1,18 @@
 App.Models.Question = Backbone.Model.extend({
+	defaults: {
+		votes: []
+	},
+
+	vote: function(userName, voteType) {
+		var voteValue = voteType === 'up' ? 1 : -1;
+		this.get('votes').push({ value: voteValue, voter: userName });
+		this.save();
+	}
+
+	voteTally: function() {
+		return this.get('votes').reduce( function(tally, vote){
+			return tally + vote.value;
+		}, 0);
+	}
 
 });
--- scripts/views/question.js
+++ scripts/views/question.js
@@ -3,8 +3,26 @@ App.Views.Question = Backbone.View.extend({
 	className: 'question',
 	template: _.template($("#question-template").html()),
 
+	events: {
+		"click .voteup" : "upVote",
+		"click .votedown" : "downVote"
+	},
+
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
+	},
+
+	upVote: function() {
+		this.setVote("up");
+	},
+
+	downVote: function() {
+		this.setVote("down");
+	},
+
+	setVote: function(voteType){
+		this.model.vote(App.currentUser.get('userName'), voteType);
 	}
+
 });
